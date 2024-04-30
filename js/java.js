e3242384f171b0925a2e2c625c9e7187326bf55c
JacksonPeake <166457017+JacksonPeake@users.noreply.github.com>
2024-04-29 18:49:24 -0600

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,7 @@
+const hamMenu = document.querySelector( '.hamMenu');
+const offScreenMenu = document.querySelector( '.offScreenMenu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active')
+    offScreenMenu.classList.toggle('active')
+})
